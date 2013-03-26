f8e0d25d6615b28eec1c25d37e8c79407538ff03
BRuNTza <drbking@gmail.com>
2013-03-26 17:55:12 +0200

    ready for a mission

---
+++ Iterator.h
@@ -0,0 +1,16 @@
+#ifndef _ITERATOR_H_
+#define _ITERATOR_H_
+
+namespace BRNDAN022{
+
+	class Iterator{
+		//private:
+			//default constructor (must be private)
+			Iterator(void);
+
+	};
+
+}
+
+
+#endif
